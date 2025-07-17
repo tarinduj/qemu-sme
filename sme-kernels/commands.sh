729fdaa41bf9fd6aad8e8a2cecbff0e396da570b
Tarindu Jayatilaka <tarindujayatilaka@mac.lan>
2025-07-16 21:13:15 -0400

    pivot

--- sme-kernels/commands.sh
+++ sme-kernels/commands.sh
@@ -7,3 +7,7 @@ make -j$(nproc)
 clang++ -march=armv9.2-a+sme2 test.cpp
 # run test
 /mnt/qemu-sme/build/qemu-aarch64 -cpu max,sme=on a.out 
+
+#512 bit/ 64 byte SVL
+clang++ -march=armv9.2-a+sme2 -O3 pivot.cpp -o pivot.bin
+/mnt/qemu-sme/build/qemu-aarch64 -cpu max,sme=on,sme-default-vector-length=64 pivot.bin 16 16 0 2 100000
